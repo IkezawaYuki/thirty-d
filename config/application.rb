72d0352721f50a6afdd65aae6a407242260dc120
yikezawa <y.ikezawa93@gmail.com>
2021-12-09 22:51:43 +0900

    mod

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module DbSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.active_record.writing_role = :writable
+    config.active_record.reading_role = :readonly
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_133636) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "entries", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "blog_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["blog_id"], name: "index_entries_on_blog_id"
-  end
-
-  add_foreign_key "entries", "blogs"
 end
